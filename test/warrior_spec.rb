133d4ee13709dfec414a10499733432bc6fc2449
itgsod-fredrik-karlen <fredrik.karlen@elev.it-gymnasiet.se>
2013-11-08 10:48:44 +0100

    added HP and Weapon

--- test/warrior_spec.rb
+++ test/warrior_spec.rb
@@ -3,13 +3,50 @@ require_relative '../lib/warrior'
 
 describe 'Warrior' do
 
-  it 'should have a name and weapon' do
-    w = Warrior.new('Baldriq', 'Sword')
-    w.weapon.should == "Sword"
+  it 'should have a name' do
+    w = Warrior.new('Baldriq')
     w.name.should == "Baldriq"
 
-    w = Warrior.new('Inigo', 'Hammer')
-    w.weapon.should == "Hammer"
+    w = Warrior.new('Inigo')
     w.name.should == "Inigo"
+  end
+
+  it 'should not have a weapon' do
+    w = Warrior.new("")
+    expect { w.weapon }.to raise_error NotImplementedError
+  end
+
+  it 'should not have hp' do
+    w = Warrior.new('')
+    expect { w.hp }.to raise_error NotImplementedError
+  end
+
+end
+
+describe "SpearWarrior" do
+
+  it 'should have a name and spear' do
+    sw = SpearWarrior.new('Inigo')
+    sw.name.should == 'Inigo'
+    sw.weapon.should == 'Spear'
+  end
+
+  it 'should have 10 hp' do
+    sw = SpearWarrior.new('')
+    sw.hp.should == 10
+  end
+
+end
+
+describe "SwordWarrior" do
+  it 'should have a name and a Sword' do
+    sw = SwordWarrior.new('Inigo')
+    sw.name.should == 'Inigo'
+    sw.weapon.should == 'Sword'
+  end
+
+  it 'should have 15 hp' do
+    sw = SwordWarrior.new('')
+    sw.hp.should == 15
   end
 end
