fe60aeb4964ed83fb1924bb301544690c916aec2
itgsod-fredrik-karlen <fredrik.karlen@elev.it-gymnasiet.se>
2013-11-08 10:08:43 +0100

    Warrior creation

---
+++ test/warrior_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require_relative '../lib/warrior'
+
+describe 'Warrior' do
+
+  it 'should have a name and weapon' do
+    w = Warrior.new('Baldriq', 'Sword')
+    w.weapon.should == "Sword"
+    w.name.should == "Baldriq"
+
+    w = Warrior.new('Inigo', 'Hammer')
+    w.weapon.should == "Hammer"
+    w.name.should == "Inigo"
+  end
+end
